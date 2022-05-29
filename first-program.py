ca79873adcaf9fcee44b73b11920311c106beedd
praveenkumarkumaresan <praveenresponsible@gmail.com>
2022-05-29 14:32:15 -0500

    first python program

---
+++ first-program.py
@@ -0,0 +1,14 @@
+num1 = input('enter the first number')
+print('first number is ',num1)
+
+num2 = input('enter the second number')
+print('second number is ',num2)
+ 
+num3= num1+num2
+
+print('sum is ',num3)
+
+num4=num1-num2
+
+print('differnce is',num4)
+
