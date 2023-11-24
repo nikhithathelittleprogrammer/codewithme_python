d15294908e6c3a55650f02a96ca735aa20d9ccca
nikhithathelittleprogrammer <nikhithapraveen2014@gmail.com>
2023-11-24 11:45:25 -0600

    add-sub-product

---
+++ add-diff-product.py
@@ -0,0 +1,42 @@
+num1 = input('enter the first number')
+print('first number is',num1)
+
+
+num2 = input('enter the second number')
+print('second number is',num2)
+
+
+# addition of two integeres
+num1= int(num1)
+num2= int(num2)
+
+num3 = num1 + num2
+print('sum is ',num3)
+
+# addition of two float
+print('sum is ',num3)
+
+num4=num1-num2
+
+print('differnce is',num4)
+#####################################################################################################
+
+num1 = float(num1)
+print('first number is after float conversion',num1)
+
+num1=num1 + num2 + .57576345890231
+print('sum is',num1)
+
+####################################################################################################
+
+num3=num1 * num2
+
+print('prudect is',num3)
+
+
+
+
+
+
+
+              
